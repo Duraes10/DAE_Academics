c0d83e95c55761cd78bca86f0ac4f03e3ff4315d
Your Name <2211091@my.ipleiria.pt>
2023-10-06 10:24:37 +0100

    Add client

---
+++ academics-client/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="error">
+    Error: {{ error.message }}
+  </div>
+  <div v-else>
+    <h2>Students</h2>
+    <div>{{ students }}</div>
+    <button @click.prevent="refresh">Refresh Data</button>
+  </div>
+</template>
+
+<script setup>
+const { data: students, error, refresh }
+    = await useFetch('http://localhost:8080/academics/api/students/all')
+</script>
